4f2e0fe349070f5444f091a332975f16d1fe4ba4
hshafiei374 <hshafiei374@gmail.com>
2021-12-24 07:50:15 +0100

    create variables for Lottery Contract

--- Lottery.sol
+++ Lottery.sol
@@ -3,5 +3,18 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Lottery{
-    
+    struct Ticket{
+        uint256 id;
+        uint256 created_at;
+        address payable member;
+        bool win;
+    }
+    address payable public owner;
+    mapping(uint256=>Ticket) public tickets;
+    uint256 public ticket_price=50 trx;
+    uint256 ticket_code=0;
+    uint256 public invested=0;
+    uint256 public start_date;
+    uint16 public day;
+    bool public is_ended=false;  
 }
